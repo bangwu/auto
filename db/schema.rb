18df5c375e42aa87243b70a800cac21bd6d88df6
wubang <wolfwubang@gmail.com>
2014-12-26 15:44:47 +0800

    add rspec test

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141225113843) do
+ActiveRecord::Schema.define(version: 20141226072645) do
+
+  create_table "students", force: true do |t|
+    t.string   "name"
+    t.integer  "number"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
